d28d1966913b8b057e8b611c4fa06ae66e9cdc2e
Sam Morrow <smorrow@oberlin.edu>
2017-06-22 15:35:42 -0400

    firebase added

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  // Change this to your firebase configuration! (Add Firebase to your web app)
+    FIREBASE_CONFIG: {
+        apiKey: "AIzaSyCWJA-YL4wQ4RV3IjB6rZbL8cmuMeMv_l8",
+        authDomain: "capstone-c9fe1.firebaseapp.com",
+        databaseURL: "https://capstone-c9fe1.firebaseio.com",
+        projectId: "capstone-c9fe1.firebaseapp.com",
+        storageBucket: "testme",
+        messagingSenderId: "416055207936"
+    },
+};
