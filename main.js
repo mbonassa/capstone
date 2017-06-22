fff19c8e80a8d8d3e6998c4ec02ffaee744b1d2d
Sam Morrow <smorrow@oberlin.edu>
2017-06-22 18:39:56 -0400

    firebase integrated

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { firebaseUsersRef } from './utils/firebase'
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      val: ""
+    }
+  }
+
+  componentDidMount(){
+    firebaseUsersRef.on("value",
+      (snapshot) => {
+      console.log("------------------------VAL-------------",snapshot)
+      this.setState({val: snapshot.val()});
+    },
+      (errorObject) => {
+      console.log("The read failed: " + errorObject.code);
+    }
+    )}
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up main.js to start working on your app!</Text>
+        <Text>{firebaseUsersRef.key} </Text>
+        <Text>{this.state.val}</Text>
       </View>
     );
   }
