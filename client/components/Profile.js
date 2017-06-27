69faa644ade7beb15946e400ccfaa1438882a7dd
Sam Morrow <smorrow@oberlin.edu>
2017-06-27 12:50:26 -0400

    small fixes to profile

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -26,16 +26,18 @@ export default class UserView extends React.Component {
   }
 
   componentDidMount(){
-    // if (firebaseAuth.currentUser){
-      firebaseUsersRef.child('User1').on("value",
+    if (firebaseAuth.currentUser){
+      firebaseUsersRef.child(firebaseAuth.currentUser.uid).on("value",
         (snapshot) => {
         this.setState({val: snapshot.val()});
       },
         (errorObject) => {
         console.log("The read failed: " + errorObject.code);
       });
+    } else {
+      alert("You are not logged in.")
     }
-  // }
+  }
 
   handleRejectPress(){
     firebaseUsersRef.child(firebaseAuth.currentUser.uid).child('viewed').update({User1: true})
@@ -51,28 +53,20 @@ export default class UserView extends React.Component {
       <div className="user-page">
       <img id="logo-top" src="./img/logo.png" />
         <div className="row default-container">
-      				<Link to={`/profile/edit`}><p style={{'color': '#ffffff'}}className='fancy-type'>EDIT INFO</p></Link>
-      			</div>
-
-
+      		<Link to={`/profile/edit`}><p style={{'color': '#ffffff'}}className='fancy-type'>EDIT INFO</p></Link>
+      	</div>
         <div>
         <img className="profile-image"src={`${this.state.val.imageUrl}`} />
         </div>
-
         <div>
           {this.state.val.name ? (<h3>{this.state.val.name}, {this.state.val.age}</h3>) : null}
-
           <hr className="horizontal-line" />
           <h5>{this.state.val.bio}</h5>
-
           <button
             title="Log out"
             onClick={this.handleLogout}
           >LOG OUT </button>
-         </div>
-
         </div>
-
       </div>
       )
   }
