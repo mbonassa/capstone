95662834a8febd00c8b99ac7ce0f9fc77f01f7ee
Marcelo Bonassa <marcelo.bonassa@gmail.com>
2017-06-27 15:22:51 -0400

    handle Quiz component state

--- client/components/Quiz.js
+++ client/components/Quiz.js
@@ -5,10 +5,14 @@ export default class Quiz extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            val: {}
+            val: {},
+            userData: {},
+            currentQuestion: {}
         }
 
     }
+
+
 
     componentDidMount () {
         firebaseQuizRef.on('value', 
@@ -18,12 +22,20 @@ export default class Quiz extends React.Component {
             (errorObject) => {
                 console.error('The read failed: ' + errorObject.code)
             })
+
+        //let user = firebaseAuth.currentUser.uid;
+        let data = firebaseUsersRef.child('User1')
+        data.on('value', 
+            (snapshot) => {
+                this.setState({userData: snapshot.val()})
+            },
+            (errorObject) => {
+                console.error('The read failed: ' + errorObject.code)
+            })
     }
 
     render() {
-        let length = this.state.val.length;
-        let random = Math.floor(Math.random() * length);
-        let question = this.state.val[random];
+        let question = this.state.currentQuestion;
         return (
             <div>
                 <h1>{question ? question[0] : null}</h1>
