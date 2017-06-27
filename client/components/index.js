8f63bd46b839dcce3f5b2edac15e8331910feaf7
Eli Nemzer <elinemzer7@gmail.com>
2017-06-27 15:48:37 -0400

    can successfully edit your profile

--- client/components/index.js
+++ client/components/index.js
@@ -1,3 +1,4 @@
-export { default as Login } from './Login'
-export { default as Signup } from './SignupForm'
-export { default as Profile } from './Profile'
+export { default as Login } from './Login';
+export { default as Signup } from './SignupForm';
+export { default as Profile } from './Profile';
+export { default as EditProfile } from './EditProfile';
