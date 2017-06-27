a6438483e5df5b9d30f2c3079b43081d33232e8d
Marcelo Bonassa <marcelo.bonassa@gmail.com>
2017-06-27 14:36:32 -0400

    feature: round 1 game basic

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-import { Profile, Login} from './components';
+import { Profile, Login, Quiz } from './components';
 
 ReactDOM.render(
    <Router history={browserHistory}>
@@ -11,6 +11,7 @@ ReactDOM.render(
         <IndexRoute component={Profile} />
         <Route path="/profile" component={Profile}/>
         <Route path="/login" component={Login}/>
+        <Route path="/quiz" component={Quiz}/>
       </Route>
     </Router>,
     document.getElementById('app')
