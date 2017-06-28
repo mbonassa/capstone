ef845ab8837eb6419f49607da0ba8e34b783aaab
Sam Morrow <smorrow@oberlin.edu>
2017-06-28 13:02:37 -0400

    some bugs with losing a partner after you enter quiz. but the run away button works and the match button makes a database entry for both

--- client/components/DailyMatch.js
+++ client/components/DailyMatch.js
@@ -12,11 +12,18 @@ export default class DailyMatch extends React.Component {
       partnerInfo: {}
     }
     this.handleMatch = this.handleMatch.bind(this);
+    this.dismissMatch = this.dismissMatch.bind(this);
+    this.enterQuiz = this.enterQuiz.bind(this);
+
+  }
+
+  enterQuiz(){
+    browserHistory.push('quiz')
   }
 
  handleMatch(){
     var q1 = Math.floor(Math.random() * 100)
-    return firebaseUsersRef.child(firebaseAuth.currentUser.uid).child('matches').child(this.state.partnerId).set({
+    firebaseUsersRef.child(firebaseAuth.currentUser.uid).child('matches').child(this.state.partnerId).set({
       heartStatus: 0,
       numbers: `${q1}, ${q1 + 1}, ${q1 - 1}, ${q1 + 2}, ${q1 - 2}`,
       round1: {},
@@ -35,12 +42,36 @@ export default class DailyMatch extends React.Component {
     })
   }
 
+  dismissMatch(){
+    firebaseUsersRef.child(firebaseAuth.currentUser.uid).update({
+        active: false,
+        partnerId: ''
+    })
+    .then(()=> {
+      firebaseUsersRef.child(this.state.partnerId).update({
+        active: false,
+        partnerId: ''
+      });
+    })
+    .then(() => {
+      browserHistory.push('profile')
+    })
+  }
+
   componentDidMount(){
     if (this.props.location.state.partnerId){
         this.setState({partnerId: this.props.location.state.partnerId}, () => {
         firebaseUsersRef.child(this.props.location.state.partnerId).on("value",
           (snapshot) => {
-          this.setState({partnerInfo: snapshot.val()});
+          this.setState({partnerInfo: snapshot.val()}, () => {
+             var partnerMatches = Object.keys(this.state.partnerInfo.matches).map(key => {
+              let objIdx = {};
+              objIdx = this.state.partnerInfo.matches[key];
+              objIdx.key = key;
+              return objIdx
+            });
+            this.setState({partnerMatches})
+          });
         },
           (errorObject) => {
           console.log("The read failed: " + errorObject.code);
@@ -66,12 +97,26 @@ export default class DailyMatch extends React.Component {
           <h5>{this.state.partnerInfo.bio}</h5>
         </div>
         <hr/>
-        <button
-        onClick={this.handleMatch}
-        >GET STARTED</button>
-        <button
-        onClick={this.dismissMatch}
-        >Run away...</button>
+      {
+        this.state.partnerMatches && this.state.partnerMatches.filter(el => {
+           return el.key === firebaseAuth.currentUser.uid
+        }).length ?
+        <div>
+          <h5> You're already in a match! </h5>
+          <button
+          onClick={this.enterQuiz}
+          >Go to your quiz...</button>
+        </div>
+        :
+        <div>
+          <button
+          onClick={this.handleMatch}
+          >GET STARTED</button>
+          <button
+          onClick={this.dismissMatch}
+          >Run away...</button>
+        </div>
+      }
       </div>
     )
   }
