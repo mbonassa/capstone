660faf22bb2c0ad59effdb36e67b13a024e2d026
Marcelo Bonassa <marcelo.bonassa@gmail.com>
2017-06-28 14:53:44 -0400

    feature: persisting quiz answers to the database

--- client/components/Quiz.js
+++ client/components/Quiz.js
@@ -28,12 +28,12 @@ export default class Quiz extends React.Component {
             })
 
         //let user = firebaseAuth.currentUser.uid;
-        let data = firebaseUsersRef.child('User1')
+        let data = firebaseUsersRef.child('User2')
         data.on('value',
             (snapshot) => {
                 this.setState({userData: snapshot.val()})
 
-                let questionNumbers = snapshot.val().matches.m1.numbers.split(',');
+                let questionNumbers = snapshot.val().matches.m3.numbers.split(',');
                 this.setState({questionNumbers})
             },
             (errorObject) => {
@@ -44,25 +44,30 @@ export default class Quiz extends React.Component {
     }
 
     findLatestMatch() {
-      var matchRef = firebaseUsersRef.child('User1').child('matches');
-        matchRef.orderByValue().limitToFirst(1).on("value", function(snapshot) {
-          snapshot.forEach(function(data) {
-          console.log("The " + data.key + " score is " + data.val().timestamp);
-          matchRef.child(data.key).update({
-            'round1': {
-              '1': '3'
-            }
-          })
-  });
-});
+
     }
 
-    handleClick() {
-      let number = this.state.current + 1;
-      this.findLatestMatch();
-      this.setState({
-        current: number
-      })
+    handleClick(event) {
+
+        let matchRef = firebaseUsersRef.child('User2').child('matches');
+        let questionNumber = +this.state.questionNumbers[this.state.current];
+        let answerNumber = Number(event.target.className);
+
+        matchRef.orderByValue().limitToFirst(1).on("value", function(snapshot) {
+            snapshot.forEach(function(data) {
+                matchRef.child(data.key).child('round1').update({
+                        [questionNumber]: [answerNumber]
+                })
+            });
+        });
+
+        let number = this.state.current + 1;
+        this.findLatestMatch();
+        this.setState({
+            current: number
+        })
+
+        
     }
 
     render() {
@@ -73,17 +78,17 @@ export default class Quiz extends React.Component {
             this.state.current < 5 ?
             <div>
                 <h1>{question ? question[0] : null}</h1>
-                <a onClick={this.handleClick}><h3>{question ? question[1] : null}</h3></a>
-                <a onClick={this.handleClick}><h3>{question ? question[2] : null}</h3></a>
-                <a onClick={this.handleClick}><h3>{question ? question[3] : null}</h3></a>
-                <a onClick={this.handleClick}><h3>{question ? question[4] : null}</h3></a>
+                <a onClick={this.handleClick}><h3 className={1}>{question ? question[1] : null}</h3></a>
+                <a onClick={this.handleClick}><h3 className={2}>{question ? question[2] : null}</h3></a>
+                <a onClick={this.handleClick}><h3 className={3}>{question ? question[3] : null}</h3></a>
+                <a onClick={this.handleClick}><h3 className={4}>{question ? question[4] : null}</h3></a>
             </div> :
             <div>
                 <h1>{question ? question[0] : null}</h1>
-                <a href='/waiting'><h3>{question ? question[1] : null}</h3></a>
-                <a href='/waiting'><h3>{question ? question[2] : null}</h3></a>
-                <a href='/waiting'><h3>{question ? question[3] : null}</h3></a>
-                <a href='/waiting'><h3>{question ? question[4] : null}</h3></a>
+                <a href='/waiting'><h3 className={1}>{question ? question[1] : null}</h3></a>
+                <a href='/waiting'><h3 className={2}>{question ? question[2] : null}</h3></a>
+                <a href='/waiting'><h3 className={3} >{question ? question[3] : null}</h3></a>
+                <a href='/waiting'><h3 className={4} >{question ? question[4] : null}</h3></a>
             </div>
             )
     }
--- client/components/Random.js
+++ client/components/Random.js
@@ -6,8 +6,10 @@ export default class Random extends React.Component {
         super(props)
         this.state = {
             data: {},
-            userData: {}
+            userData: {},
+            boolean: true
         }
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount () {
@@ -33,16 +35,20 @@ export default class Random extends React.Component {
 
     }
 
+    handleClick () {
+        this.setState({boolean: false});
+    }
+
     render() {
         let number = Math.round(Math.random() * this.state.data.length) + 1;
         let question = this.state.data[number];
         return (
             <div>
                 <h1>{question ? question[0] : null}</h1>
-                <a><h3>{question ? question[1] : null}</h3></a>
-                <a><h3>{question ? question[2] : null}</h3></a>
-                <a><h3>{question ? question[3] : null}</h3></a>
-                <a><h3>{question ? question[4] : null}</h3></a>
+                <a onClick={this.handleClick}><h3>{question ? question[1] : null}</h3></a>
+                <a onClick={this.handleClick}><h3>{question ? question[2] : null}</h3></a>
+                <a onClick={this.handleClick}><h3>{question ? question[3] : null}</h3></a>
+                <a onClick={this.handleClick}><h3>{question ? question[4] : null}</h3></a>
             </div>
         )
     }
