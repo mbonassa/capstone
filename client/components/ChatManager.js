cf5fd04988b48d9eb3876b8e1ef8bbe3f5d7dfe1
Sam Morrow <smorrow@oberlin.edu>
2017-07-03 18:52:34 -0400

    comments added to chat

--- client/components/ChatManager.js
+++ client/components/ChatManager.js
@@ -11,6 +11,7 @@ import Chat from './Chat'
 // This component is a little piece of glue between React router
 // and our Scratchpad component. It takes in props.params.title, and
 // shows the Scratchpad along with that title.
+
 export default class ChatManager extends React.Component {
     constructor(props){
         super(props);
@@ -26,18 +27,16 @@ export default class ChatManager extends React.Component {
     componentDidMount(){
         firebaseAuth.onAuthStateChanged(user => {
             if (user) {
-                console.log('firing')
+                //grab user info and partner info
                 firebaseUsersRef.child(firebaseAuth.currentUser.uid).on('value', snapshot => {
-                this.setState({userInfo: snapshot.val()}, () => {
-                    console.log(this.state)
-                    })
+                    this.setState({userInfo: snapshot.val()})
                 })
                 firebaseUsersRef.child(this.props.params.partnerId).on('value', snapshot => {
-                this.setState({partnerInfo: snapshot.val()}, () => {
-                    console.log('changing')
-                    this.userId = firebaseAuth.currentUser.uid
-                    this.setState({})
-                })
+                    this.setState({partnerInfo: snapshot.val()}, () => {
+                        this.userId = firebaseAuth.currentUser.uid
+                        // have to trigger a rerender, so...
+                        this.setState({})
+                    })
                 });
             }
         })
@@ -48,6 +47,7 @@ export default class ChatManager extends React.Component {
         firebaseUsersRef.child(this.props.params.partnerId).off();
     }
 
+    // let's only render chat after we've received proper info from our db!
     renderChat(){
      return <Chat
           auth={auth}
@@ -59,12 +59,7 @@ export default class ChatManager extends React.Component {
     render(){
         return (
       <div>
-        <h1>Your chat with {this.state.partnerInfo.name ? this.state.partnerInfo.name : 'Bob'}</h1>
-        {/* Here, we're passing in a Firebase reference to
-            /scratchpads/$scratchpadTitle. This is where the scratchpad is
-            stored in Firebase. Each scratchpad is just a string that the
-            component will listen to, but it could be the root of a more complex
-            data structure if we wanted. */}
+        <h1>Your chat with {this.state.partnerInfo.name ? this.state.partnerInfo.name : 'a mysterious stranger'}</h1>
         {this.userId ? <div> {this.renderChat()} </div>: null}
         <Link to="/profile"><p className="caps back"><span className="glyphicon glyphicon-chevron-left"></span>back to profile</p></Link>
       </div>
