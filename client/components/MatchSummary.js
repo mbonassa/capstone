8d858d4fe4b74f54d61432bfa8e606b6bb34cbeb
Eli Nemzer <elinemzer7@gmail.com>
2017-07-05 17:48:30 -0400

    making progress

--- client/components/MatchSummary.js
+++ client/components/MatchSummary.js
@@ -14,27 +14,49 @@ export default class MatchSummary extends React.Component {
           allMatches: {},
           myAnswers: [],
           theirAnswers: [],
-          heartStatus: 0,
           theirName: '',
           finishedQuiz: true,
           userObj: {},
           allUsersObj: '',
-          quizData: {}
+          quizData: {},
+          data: {}
       }
+      this.partnerId = this.props.params.partnerId || {};
     }
 
     componentDidMount(){
+      firebaseUsersRef.on('value', snapshot => {
+        this.setState({
+          allUsersObj: snapshot.val()
+        })
+      })
+
        firebaseAuth.onAuthStateChanged((user) => {
         if (user) {
-      firebaseUsersRef.child(firebaseAuth.currentUser.uid).on('value', snapshot => {
+      firebaseUsersRef.child(firebaseAuth.currentUser.uid).on('value',
+        snapshot => {
         this.setState({userObj: snapshot.val()}, () => {
-          firebaseUsersRef.on('value', snapshot => {
-            this.setState({allUsersObj: snapshot.val()})
-          })
-        })
+              firebaseUsersRef.child(firebaseAuth.currentUser.uid).child('matches').on('value',
+              (snapshot) => {
+                  this.setState({
+                      questionNumbers: snapshot.val().numbers.split(',')
+                  })
+              });
+          }
+        )
       });
         }
        });
+
+       firebaseQuizRef.on('value',
+           (snapshot) => {
+               this.setState({
+                 data: snapshot.val()
+               })
+           },
+           (errorObject) => {
+               console.error('The read failed: ' + errorObject.code)
+           })
     }
 
     componentWillUnmount(){
@@ -46,8 +68,12 @@ export default class MatchSummary extends React.Component {
     let questionNumbers = this.state.questionNumbers;
     let question = questionNumbers ? this.state.data[questionNumbers[this.state.current]] : null;
       console.log('jen', this.state.userObj)
-      console.log(questionNumbers)
-      console.log('companero', this.partnerId)
+      console.log('question', question)
+      console.log('data', this.state.data)
+      console.log('qnumbers', questionNumbers)
+      console.log('partner', this.partnerId.numbers)
+
+
     return (
       <div>
         <img className="logo-top" src="./img/sm-logo.png" />
@@ -55,13 +81,14 @@ export default class MatchSummary extends React.Component {
           <div>
           {
             arrayifyWithKey(this.state.userObj.matches).map(match => {
-            console.log('match', match)
+            console.log('match', (this.state.allUsersObj[match.key]))
             return (
                 <div key={match.key}>
 
-          <h3 className="inline-block"> Compatibility Cheat Sheet </h3>
-          <p>In case you forgot...</p>
+                <h3 className="inline-block"> Compatibility Cheat Sheet </h3>
+                <p>In case you forgot...</p>
 
+                <p>{this.state.allUsersObj && (this.state.allUsersObj[match.key]).matches.numbers}</p>
 
 
 
@@ -84,7 +111,7 @@ export default class MatchSummary extends React.Component {
            }
         </div> :
         <div>
-          <span>blahblahblah</span>
+          <span></span>
         </div>
       }
       </div>
